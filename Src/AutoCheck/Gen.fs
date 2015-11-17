9f41ab11d469bcf4394b0e3f3d588f2524da1210
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2015-11-17 12:09:38 +0200

    Added gen as Computation Expression, to verify that the apply and return (init) types check.

--- Src/AutoCheck/Gen.fs
+++ Src/AutoCheck/Gen.fs
@@ -56,3 +56,15 @@ module Operators =
     let (>>=) m f = bind m f
     let (<*>) f m = apply f m
     let (<!>) f m = map f m
+
+[<AutoOpen>]
+module Builder =
+    type GenBuilder() =
+        member this.Bind       (m1, m2) = bind m1 m2
+        member this.Combine    (m1, m2) = bind m1 (fun () -> m2)
+        member this.Delay      (f)      = bind (init()) f
+        member this.Return     (x)      = init x
+        member this.ReturnFrom (f)      = f
+        member this.Zero       ()       = init()
+
+    let gen = GenBuilder()
