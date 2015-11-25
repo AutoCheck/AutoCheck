be910e6b78da6c9acbb5bb05d7a043b0258c2a38
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2015-11-25 16:38:50 +0200

    Ported Random.randomR, based on randomIvalInteger function, as defined in System.Random 1.1.
    See also: https://hackage.haskell.org/package/random-1.1

--- Src/AutoCheck/Random.fs
+++ Src/AutoCheck/Random.fs
@@ -7,17 +7,19 @@ type StdGen =
     private
     | StdGen of int * int
 
+let private next (StdGen (s1, s2)) =
+    let k    = s1 / 53668
+    let k'   = s2 / 52774
+    let s1'  = 40014 * (s1 - k * 53668)  - k * 12211
+    let s2'  = 40692 * (s2 - k' * 52774) - k' * 3791
+    let s1'' = if s1' < 0 then s1' + 2147483563 else s1'
+    let s2'' = if s2' < 0 then s2' + 2147483399 else s2'
+    let z    = s1'' - s2''
+    let z'   = if z < 1 then z + 2147483562 else z
+
+    (z', StdGen (s1'', s2''))
+
 let split (StdGen (s1, s2) as std) =
-    let next (StdGen (s1, s2)) =
-        let k    = s1 / 53668
-        let k'   = s2 / 52774
-        let s1'  = 40014 * (s1 - k * 53668)  - k * 12211
-        let s2'  = 40692 * (s2 - k' * 52774) - k' * 3791
-        let s1'' = if s1' < 0 then s1' + 2147483563 else s1'
-        let s2'' = if s2' < 0 then s2' + 2147483399 else s2'
-        let z    = s1'' - s2''
-        let z'   = if z < 1 then z + 2147483562 else z
-        (z', StdGen (s1'', s2''))
     let s1' = if s1 = 2147483562 then 1 else s1 + 1
     let s2' = if s2 = 1 then 2147483398 else s2 - 1
     let (StdGen (t1, t2)) = next std |> snd
@@ -32,4 +34,21 @@ let createStdGen i =
     StdGen (s1 + 1, s2 + 1)
 
 let randomSeed () = int32 DateTime.UtcNow.Ticks
-let randomR a g = (fst a, g)
+
+let rec randomR (l, h) rng =
+    if l > h then randomR (h, l) rng
+    else
+        let (l', h')    = (Int32.MinValue, Int32.MaxValue)
+        let b           = h' - l' + 1
+        let q           = 1000
+        let k           = h - l + 1
+        let magnitude   = k * q
+        let rec f c v g =
+            if c >= magnitude then (v, g)
+            else
+                let (x, g') = next g
+                let v'      = (v * b + (x - l'))
+                f (c * b) v' g'
+        let (v, rng') = f 1 0 rng
+
+        (l + v % k), rng'
