280f844941e76a85fa5c6e8f6e44730b237d80d3
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2016-01-09 20:49:03 +0200

    Added Property.forAll.

--- Src/AutoCheck.UnitTests/PropertyTests.fs
+++ Src/AutoCheck.UnitTests/PropertyTests.fs
@@ -1,3 +1,40 @@
 ﻿module AutoCheck.UnitTests.PropertyTests
 
+open Xunit
+open Swensen.Unquote
 
+open AutoCheck
+open AutoCheck.Property
+
+[<Fact>]
+let ``ForAll returns correct result for properties that are true`` () =
+    let g = Gen.list Gen.int32
+    let prop = fun xs -> xs |> List.rev |> List.rev = xs
+
+    let actual =
+        prop
+        |> Property.forAll g
+        |> Property.evaluate
+        |> Gen.sample
+
+    test <@ actual
+            |> Seq.map (fun r -> r.Status)
+            |> Seq.choose id
+            |> Seq.forall id @>
+
+[<Fact>]
+let ``ForAll returns correct result for properties that are false`` () =
+    let g = Gen.list Gen.int32
+    let prop = fun xs -> xs |> List.rev = xs
+
+    let actual =
+        prop
+        |> Property.forAll g
+        |> Property.evaluate
+        |> Gen.sample
+
+    test <@ actual
+            |> Seq.map (fun r -> r.Status)
+            |> Seq.choose id
+            |> Seq.forall id
+            |> not @>
--- Src/AutoCheck/Property.fs
+++ Src/AutoCheck/Property.fs
@@ -1,1 +1,44 @@
 ﻿module AutoCheck.Property
+
+open AutoCheck.Gen
+
+type Property =
+    private
+    | Prop of Gen<Result>
+
+and Result =
+    { Status : option<bool>
+      Stamp  : list<string>
+      Args   : list<string> }
+
+let evaluate property =
+    let (Prop result) = property
+    result
+
+let private boolProperty a =
+    { Status = Some a
+      Stamp  = []
+      Args   = [] }
+    |> Gen.init
+    |> Prop
+
+let private unitProperty =
+    { Status = None
+      Stamp  = []
+      Args   = [] }
+    |> Gen.init
+    |> Prop
+
+let private toProperty candidate =
+    match box candidate with
+    | :? bool as b -> boolProperty b
+    | _            -> unitProperty
+
+let forAll g f =
+    Prop(gen {
+             let! arg = g
+             let! res = f arg
+                        |> toProperty
+                        |> evaluate
+             return { res with Args = arg.ToString() :: res.Args }
+         })
