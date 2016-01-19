ba3ea8f0aecf82a6bbce8d0a16cc37ebe527ffef
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2016-01-19 23:43:14 +0200

    Added Config.quick.

--- Src/LightCheck/Config.fs
+++ Src/LightCheck/Config.fs
@@ -5,3 +5,17 @@ type Config =
       MaxFail : int
       Size    : int -> int
       Every   : int -> List<string> -> string }
+
+let quick =
+    { MaxTest = 100
+      MaxFail = 1000
+      Size    = fun n -> 3 + n / 2
+      Every   =
+          fun n args ->
+              let s = n.ToString()
+
+              let a =
+                  [ for c in s -> char System.Environment.NewLine ]
+                  |> Array.ofList
+                  |> System.String
+              s + a }
