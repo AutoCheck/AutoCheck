416fdcac56eb87f09cc729703dba8187b74fd151
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2016-01-22 15:02:09 +0200

    Simplified Config.quick's Every function implementation.

--- Src/LightCheck/Config.fs
+++ Src/LightCheck/Config.fs
@@ -12,15 +12,7 @@ let quick =
     { MaxTest = 100
       MaxFail = 1000
       Size    = fun n -> 3 + n / 2
-      Every   =
-          fun n args ->
-              let s = n.ToString()
-
-              let a =
-                  [ for c in s -> char Environment.NewLine ]
-                  |> Array.ofList
-                  |> System.String
-              s + a }
+      Every   = fun n args -> sprintf "%s\b" (n.ToString()) }
 
 let verbose =
     { quick with Every =
