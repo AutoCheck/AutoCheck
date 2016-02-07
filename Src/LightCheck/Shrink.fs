61039ea60d56b60d1f7aba64c03e64f99a21b532
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2016-02-07 20:58:56 +0200

    Introduced Shrink module.

---
+++ Src/LightCheck/Shrink.fs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// This module deals with simplifying counter-examples. A property fails when
+/// LightCheck finds a first counter-example. However, randomly-generated data
+/// typically contains a lot of noise. Therefore it is a good idea to simplify
+/// counter-examples before reporting them. This process is called shrinking.
+///
+/// Read more about how it works here:
+/// http://www.dcc.fc.up.pt/~pbv/aulas/tapf/slides/quickcheck.html#shrinking
+/// </summary>
+module LightCheck.Shrink
